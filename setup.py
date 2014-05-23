5bb5b572770ebce2921cade0a75608c1a7ffbe69
Jon Crall <crallj@rpi.edu>
2014-05-23 19:51:29 -0400

    wip

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 from __future__ import absolute_import, division, print_function
 from utool.util_setup import setuptools_setup
-import detecttools
 
 
 if __name__ == '__main__':
     setuptools_setup(
         setup_fpath=__file__,
-        module=detecttools,
+        name='detecttools',
+        version='1.0.0.dev1',
         description=('Utilities for writing detectors (like pyrf)'),
         url='https://github.com/bluemellophone/pyrf',
         author='Jason Parham',
